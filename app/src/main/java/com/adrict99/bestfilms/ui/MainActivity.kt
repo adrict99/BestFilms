cc0e2d35bc26cc31e788e370baa9973ad2d0f8d8
adrict99 <adrian.castro@digio.es>
2022-06-08 20:36:12 +0200

    Added styles, colors, strings and refactored some code in MainActivity.kt

--- app/src/main/java/com/adrict99/bestfilms/ui/MainActivity.kt
+++ app/src/main/java/com/adrict99/bestfilms/ui/MainActivity.kt
@@ -3,7 +3,8 @@ package com.adrict99.bestfilms.ui
 import android.os.Bundle
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.NavigationUiSaveStateControl
 import com.adrict99.bestfilms.R
 import com.adrict99.bestfilms.databinding.ActivityMainBinding
 import com.adrict99.bestfilms.ui.common.BaseActivity
@@ -17,6 +18,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     lateinit var viewModelFactory: ViewModelFactory<MainViewModel>
     val mainViewModel: MainViewModel by lazy { viewModelFactory.get() }
 
+    @OptIn(NavigationUiSaveStateControl::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
@@ -25,20 +27,9 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.main_nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.findNavController()
 
-        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController, false)
+        setupWithNavController(binding.bottomNavigationView, navController, false)
     }
 
     override fun getViewBinding(): ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
 
-//    private fun setupBottomNavigationView() {
-//        val bottomNavigationView = binding.bottomNavigationView
-//        val navGraphs = listOf(R.navigation.nav_graph)
-//
-//        val controller = bottomNavigationView.setupWithNavController(
-//            navGraphs,
-//            supportFragmentManager,
-//            R.id.main_nav_host_fragment,
-//            intent
-//        )
-//    }
 }
