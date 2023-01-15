597bcb6c26c3d6cafd6625fa3c3f5198f4d7de6c
adrict99 <adrian.castro@digio.es>
2023-01-15 17:02:09 +0100

    Modified a few movie details

--- app/src/main/java/com/adrict99/bestfilms/utils/extensions/MovieDetailExtensions.kt
+++ app/src/main/java/com/adrict99/bestfilms/utils/extensions/MovieDetailExtensions.kt
@@ -2,8 +2,6 @@ package com.adrict99.bestfilms.utils.extensions
 
 import com.adrict99.bestfilms.domain.model.detail.Country
 import com.adrict99.bestfilms.domain.model.detail.Genre
-import com.adrict99.bestfilms.domain.model.detail.Language
-import com.adrict99.bestfilms.utils.extra.CountryFlags
 import java.math.BigDecimal
 import java.math.RoundingMode
 import java.util.*
@@ -11,11 +9,9 @@ import java.util.*
 fun Double.round(decimals: Int): Double =
     BigDecimal(this).setScale(decimals, RoundingMode.HALF_EVEN).toDouble()
 
-fun Int.asMinutes(): String = if (this == 1) "$this min" else "$this mins"
+fun Int.asMinutes(): String = "$this min."
 
 fun List<Genre>.genreJoinToString(): String = this.joinToString { it.name }
-
-fun List<Language>.languageJoinToString(): String = this.joinToString { it.name }
 
 fun List<Country>.toUnicodeFlag(): String =
     this.joinToString { countryCodeToEmojiFlag(it.iso31661) }
